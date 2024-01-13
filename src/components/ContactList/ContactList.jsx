07f7ef44f53d347ef5a663cabddbd02f9bb8ed70
Maciek <maciekjozwicki@wp.pl>
2024-01-14 00:57:08 +0100

    Update ContactList.jsx

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,25 +1,7 @@
-import { useEffect } from 'react';
-import { useState } from 'react';
+
 
 const ContactList = ({ filteredContacts, handleRemoveContact }) => {
-  // const [personData, setpersonData] = useState([]); // powielone
-
-  // useEffect(() => {
-  //   let array = contacts;
-  //   array = array === null ? [] : array;
-  //   localStorage.setItem('person', JSON.stringify(array)); // powielone
-
-  //   setpersonData(array);
-  // }, [contacts]);
-
-  // const handleDelete = id => {
-  //   const updateContact = personData.filter(person => person.id !== id);
-  //   localStorage.setItem('person', JSON.stringify(updateContact));
-  //   setpersonData(updateContact);
-  //   if (updateContact.length === 0) {
-  //     localStorage.removeItem('person');
-  //   }
-  // };
+ 
 
   console.log(22, filteredContacts);
 
